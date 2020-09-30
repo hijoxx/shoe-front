e6440dc9f4de4d7ba86a98d9eb2d99f09ddb35a5
Hijoxx <mr.leroyflorian@gmail.com>
2020-09-30 11:54:20 +0200

    ombre sur la box / test pour le filtre svg

--- src/components/Homepage/ButtonProduct/ButtonProduct.jsx
+++ src/components/Homepage/ButtonProduct/ButtonProduct.jsx
@@ -2,23 +2,20 @@ import React, {} from 'react'
 
 function ButtonProduct({children}) {
 
-
-
     const style = {
         buttonProduct : {
-            backgroundColor: "white",
-            color : "black",
+            backgroundColor : "#fefefe",
+            filter: "url(/#noise)",
             borderStyle : "none",
             height : "80px",
             width : "250px",
-            fontSize : "1.3rem",
-            filter : "url(/#noise)"
-
+            boxShadow: "10px 10px 50px -13px rgba(0,0,0,0.75)",
         },
         textProduct :{
-        filter : "url()"
-    }
-
+            color : "black",
+            fontSize : "1.3rem",
+            filter: "url(/#noNoise)"
+        }
     }
 
 
@@ -27,6 +24,10 @@ function ButtonProduct({children}) {
         <svg viewBox="0 0 0 0">
             <filter id='noise' x='0%' y='0%' width='100%' height='100%'>
                 <feTurbulence baseFrequency="2 0.04" result="NOISE" numOctaves="2"/>
+                <feDisplacementMap in="SourceGraphic" in2="NOISE" scale="20" xChannelSelector="R" yChannelSelector="R"/>
+            </filter>
+            <filter id='noNoise' x='0%' y='0%' width='100%' height='100%'>
+                <feTurbulence baseFrequency="0 0" result="NOISE" numOctaves="2"/>
                 <feDisplacementMap in="SourceGraphic" in2="NOISE" scale="20" xChannelSelector="R" yChannelSelector="R"/>
             </filter>
         </svg>
